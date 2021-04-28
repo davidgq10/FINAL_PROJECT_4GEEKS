ad0bec2e80eb0a2f9d75c1b9d03ec8f4f9b987af
davidgq10 <davidgq10@hotmail.com>
2021-04-28 02:01:08 +0000

    prueba homed

---
+++ src/front/js/pages/homed.js
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import rigoImageUrl from "../../img/rigo-baby.jpg";
+import "../../styles/home.scss";
+import { Jumbotron, Button } from "react-bootstrap";
+
+export const Homed = () => {
+	return (
+		<Jumbotron>
+			<h1>Hello, world!</h1>
+			<p>
+				This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured
+				content or information.
+			</p>
+			<p>
+				<Button variant="primary">Learn more</Button>
+			</p>
+		</Jumbotron>
+	);
+};
