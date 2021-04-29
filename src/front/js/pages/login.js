f0dc3ad95514cc87f930f54c02b90cb6f26b4fbc
mau2662 <vargas7384@gmail.com>
2021-04-29 02:38:02 +0000

    footer

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -5,16 +5,12 @@ import "../../styles/home.scss";
 
 export const Login = () => {
 	return (
-		<div className="card">
-			<img src="..." className="card-img-top" alt="..." />
-			<div className="card-body">
-				<h5 className="card-title">Card title</h5>
-				<p className="card-text">
-					Some quick example text to build on the card title and make up the bulk of the card content.
-				</p>
-				<a href="#" className="btn btn-primary">
-					Go somewhere
-				</a>
+		<div className="container-fluid bg-info">
+			<h1 className="text-left pl-0"> Mauricio APP</h1>
+			<div className="row">
+				<button type="button" classNameName="btn btn-primary ml-5" col-sm>
+					Are new? Please Login Here!!
+				</button>
 			</div>
 		</div>
 	);
