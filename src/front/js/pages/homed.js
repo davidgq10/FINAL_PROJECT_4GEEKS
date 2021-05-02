012059cb42a2b1d7a391a8d9ba61fc04e9a7db78
davidgq10 <davidgq10@hotmail.com>
2021-05-02 06:47:38 +0000

    se añaden imágenes y se configuran las card reponsivas

--- src/front/js/pages/homed.js
+++ src/front/js/pages/homed.js
@@ -1,72 +1,79 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import icBuscar from "../../img/buscar.png";
+import icTienda from "../../img/tienda.png";
+import icSoporte from "../../img/consultante.png";
+import icMan from "../../img/man.png";
 import "../../styles/home.scss";
 
 export const Homed = () => {
 	return (
-		<div>
+		<div className="myHome">
 			{/*Sección de Jumbotron*/}
-			<div className="container-fluid">
-				<h1 className="text-primary">Buscas un repuesto???</h1>
-				<p className="text-ColorAzulOscuro">
-					En este sitio podrás encontrar los repuestos disponibles para tu vehículo por parte de los
-					diferentes distribuidores del país
-				</p>
+			<div className="container">
+				<div className="transbox">
+					<h1 className="text-primary text-center">¿Necesitas un repuesto para tu vehículo?</h1>
+					<h4 className="text-ColorAzulOscuro text-center">
+						En este sitio podrás encontrar los repuestos disponibles de los distribuidores del país
+					</h4>
+				</div>
 				{/*Sección de Cards*/}
 				<div className="container">
-					<div className=" row justify-content-md-center">
-						<div className="card-deck">
-							<div className="card" style={{ width: "18rem" }}>
-								<img
-									src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgMgyoZcAFjcYu6mZKjmqXEFeoS8lDZt2egw&usqp=CAU"
-									className="card-img-top img-m50"
-									alt="..."
-								/>
-								<div className="card-body">
-									<h5 className="card-title">Card title</h5>
-									<a href="#" className="btn btn-primary">
-										Go somewhere
-									</a>
+					<div className="row justify-content-md-center">
+						<div className="card-deck card-columns">
+							<div
+								className="card shadow-lg bg-ColorAzulClaro border border-dark pb-0"
+								style={{ width: "16rem", height: "25rem" }}>
+								<div className="container-fluid">
+									<img
+										src={icBuscar}
+										className="card-img-top img-fluid pt-4 pb-3 rounded-top"
+										alt="..."
+									/>
+								</div>
+
+								<div className="card-body  bg-light">
+									<p className="card-text text-center">
+										En esta sección podrás encontrar tu repuesto aplicando criterios de búsqueda de
+										tu vehículo
+									</p>
 								</div>
 							</div>
-							<div className="card" style={{ width: "18rem" }}>
-								<img
-									src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgMgyoZcAFjcYu6mZKjmqXEFeoS8lDZt2egw&usqp=CAU"
-									className="card-img-top img-m50"
-									alt="..."
-								/>
+							<div className="card shadow-lg pt-3" style={{ width: "16rem", height: "25rem" }}>
+								<div className="container-fluid mt-1 bg-ColorAzulClaro">
+									<img src={icTienda} className="card-img-top img-fluid p-2" alt="..." />
+								</div>
+
 								<div className="card-body">
-									<h5 className="card-title">Card title</h5>
-									<a href="#" className="btn btn-primary">
-										Go somewhere
-									</a>
+									<p className="card-text text-center">
+										En esta sección podrás inscribirte como anunciante
+									</p>
 								</div>
 							</div>
-							<div className="card" style={{ width: "18rem" }}>
-								<img
-									src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgMgyoZcAFjcYu6mZKjmqXEFeoS8lDZt2egw&usqp=CAU"
-									className="card-img-top img-m50"
-									alt="..."
-								/>
+							<div className="card shadow-lg pt-3" style={{ width: "16rem", height: "25rem" }}>
+								<div className="container-fluid mt-1 bg-ColorAzulOscuro">
+									<img src={icMan} className="card-img-top img-fluid p-2" alt="..." />
+								</div>
+
 								<div className="card-body">
-									<h5 className="card-title">Card title</h5>
-									<a href="#" className="btn btn-primary">
-										Go somewhere
-									</a>
+									<p className="card-text text-center">
+										Registrate y obtén la posibilidad de realizar compras en línea y gestionar tus
+										artículos
+									</p>
 								</div>
 							</div>
-							<div className="card" style={{ width: "18rem" }}>
-								<img
-									src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgMgyoZcAFjcYu6mZKjmqXEFeoS8lDZt2egw&usqp=CAU"
-									className="card-img-top img-m50"
-									alt="..."
-								/>
+							<div
+								className="card shadow-lg pt-3 bg-ColorAzulOscuro "
+								style={{ width: "16rem", height: "25rem" }}>
+								<div className="container-fluid mt-1">
+									<img src={icSoporte} className="card-img-top img-fluid p-2" alt="..." />
+								</div>
+
 								<div className="card-body">
-									<h5 className="card-title">Card title</h5>
-									<a href="#" className="btn btn-primary">
-										Go somewhere
-									</a>
+									<p className="card-text text-center">
+										Si necesitas ayuda de un profesional para saber cuál repuesto necesitas puedes
+										consultar aquí
+									</p>
 								</div>
 							</div>
 						</div>
