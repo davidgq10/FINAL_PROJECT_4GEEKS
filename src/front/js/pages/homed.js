2f426a910b1a9336f3d903437b222b3e3f1b6d8b
davidgq10 <davidgq10@hotmail.com>
2021-05-07 02:59:55 +0000

    se repara vinculo

--- src/front/js/pages/homed.js
+++ src/front/js/pages/homed.js
@@ -8,38 +8,38 @@ import icMan from "../../img/man.png";
 import "../../styles/home.scss";
 
 export const Homed = () => {
-	// vínculo a soporte
-	const externalLinkSoporte = () =>
-		window.open("https://api.whatsapp.com/send?phone=+50684495779&text=Hola,%20como%20estas?");
-
 	// se crea un array de objetos que contiene las opciones del menú
 	const Menu = [
 		{
 			imagen: icBuscar,
 			descripcion: "En esta sección podrás encontrar tu repuesto aplicando criterios de búsqueda de tu vehículo",
-			vista: "/product"
+			vista: "/product",
+			esVista: "true"
 		},
 		{
 			imagen: icTienda,
 			descripcion: "En esta sección podrás inscribirte como anunciante",
-			vista: "/"
+			vista: "/register",
+			esVista: "true"
 		},
 		{
 			imagen: icMan,
 			descripcion: "Registrate y obtén la posibilidad de realizar compras en línea y gestionar tus artículos",
-			vista: "/"
+			vista: "/login",
+			esVista: "true"
 		},
 		{
 			imagen: icSoporte,
 			descripcion:
 				"Si necesitas ayuda de un profesional para saber cuál repuesto necesitas puedes consultar aquí",
-			vista: externalLinkSoporte
+			vista: "https://api.whatsapp.com/send?phone=+50684495779&text=Hola,%20como%20estas?",
+			esVista: "false"
 		}
 	];
 
 	// en el siguiente bloque se generan las tarjetas
 	const getCards = Menu.map((item, index) => {
-		return (
+		return item.esVista == "true" ? (
 			<Link key={index} to={item.vista} classclassName="text-link">
 				<div
 					className="text-link card order border-dark border-2 pb-0"
@@ -52,6 +52,24 @@ export const Homed = () => {
 					</div>
 				</div>
 			</Link>
+		) : (
+			<a
+				key={index}
+				href={item.vista}
+				target="_blank"
+				rel="noopener noreferrer"
+				className="text-link card order border-dark border-2 pb-0"
+				style={{ width: "15rem", height: "25rem", textDecoration: "none" }}>
+				<div className="container-fluid">
+					<img src={item.imagen} className="card-img-top img-fluid pt-4 pb-3 rounded-top" alt="..." />
+				</div>
+				<div className="card-body bg-light" style={{ "min-height": "8rem" }}>
+					<p className="card-text text-center">
+						{item.descripcion}
+						{item.esVista.toString()}
+					</p>
+				</div>
+			</a>
 		);
 	});
 
