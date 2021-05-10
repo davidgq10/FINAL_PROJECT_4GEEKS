64b96a136c4db05690b8057b5ed1cdcedfbf0703
davidgq10 <davidgq10@hotmail.com>
2021-05-10 02:44:24 +0000

    se modifica delay en home para las card

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -7,9 +7,11 @@ export const Navbar = () => {
 			<Link to="/">
 				<span className="navbar-brand mb-0 ">Inicio</span>
 			</Link>
+			<span className="navbar-brand mb-0 ">|</span>
 			<Link to="/login">
 				<span className="navbar-brand mb-0 ">Login</span>
 			</Link>
+			<span className="navbar-brand mb-0 ">|</span>
 			<Link to="/product">
 				<span className="navbar-brand mb-0 ">Productos</span>
 			</Link>
